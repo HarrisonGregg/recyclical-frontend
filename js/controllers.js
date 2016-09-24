393bf3c8f9033f66ed60846458b505e720826fcb
Harrison Gregg <animasterh@gmail.com>
2016-09-24 16:33:39 -0400

    Initial commit

---
+++ js/controllers.js
@@ -0,0 +1,74 @@
+angular.module('app.controllers', [])
+  
+.controller('recyclicalCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('whereToRecycleCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('nearbyLocationsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('locationCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('zeroWasteNYCCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('recycling101Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('compostingCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('aboutRecyclicalCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ js/routes.js
@@ -0,0 +1,103 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('menu.recyclical', {
+    url: '/',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/recyclical.html',
+        controller: 'recyclicalCtrl'
+      }
+    }
+  })
+
+  .state('menu', {
+    url: '/menu',
+    templateUrl: 'templates/menu.html',
+    controller: 'menuCtrl'
+  })
+
+  .state('menu.whereToRecycle', {
+    url: '/where-to-recycle',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/whereToRecycle.html',
+        controller: 'whereToRecycleCtrl'
+      }
+    }
+  })
+
+  .state('menu.nearbyLocations', {
+    url: '/locations',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/nearbyLocations.html',
+        controller: 'nearbyLocationsCtrl'
+      }
+    }
+  })
+
+  .state('menu.location', {
+    url: '/location',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/location.html',
+        controller: 'locationCtrl'
+      }
+    }
+  })
+
+  .state('menu.zeroWasteNYC', {
+    url: '/zero-waste-nyc',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/zeroWasteNYC.html',
+        controller: 'zeroWasteNYCCtrl'
+      }
+    }
+  })
+
+  .state('menu.recycling101', {
+    url: '/recycling-101',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/recycling101.html',
+        controller: 'recycling101Ctrl'
+      }
+    }
+  })
+
+  .state('menu.composting', {
+    url: '/waste-type',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/composting.html',
+        controller: 'compostingCtrl'
+      }
+    }
+  })
+
+  .state('menu.aboutRecyclical', {
+    url: '/about',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/aboutRecyclical.html',
+        controller: 'aboutRecyclicalCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/menu/')
+
+  
+
+});
